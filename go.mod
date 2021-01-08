a13e04ff8b9e018fad32ff7b6ffb2eeb8fc2ba47
shunak <shuna966@gmail.com>
2021-01-08 13:06:49 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/GoogleCloudPlatform/golang-samples/cloudsql/mysql/database-sql
+
+go 1.13
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/natureglobal/realip v0.0.1
+	github.com/pion/webrtc/v2 v2.2.26
+	github.com/pion/webrtc/v3 v3.0.3 // indirect
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "log"
+  "os"
+
+  "github.com/pion/webrtc/v3"
+)
+
+func main() {
+  config := webrtc.Configuration{
+    ICEServers: []webrtc.ICEServer{
+      {
+        URLs: []string{"stun:stun.l.google.com:19302"},
+      },
+    },
+  }
+
+  // generate a new connection
+  peerConnection, err := webrtc.NewPeerConnection(config)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  peerConnection.OnICECandidate(func(c *webrtc.ICECandidate) {
+    if c == nil {
+      os.Exit(0)
+    }
+    switch c.Typ {
+    case webrtc.ICECandidateTypeHost:
+      log.Println("Local IP Address:", c.Address)
+    case webrtc.ICECandidateTypeSrflx:
+      log.Println("Public IP Address:", c.Address)
+    }
+  })
+
+  if _, err := peerConnection.CreateDataChannel("", nil); err != nil {
+    log.Fatal(err)
+  }
+
+  offer, err := peerConnection.CreateOffer(nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  if err = peerConnection.SetLocalDescription(offer); err != nil {
+    log.Fatal(err)
+  }
+
+  // block forever
+  select {}
+}
